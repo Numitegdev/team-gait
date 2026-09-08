b5a0c1e3de94deed9dc04589599e4984e62d8c66
Numitegdev <numiteg.itchannel@gmail.com>
2026-09-08 11:00:49 +0700

    Update scanner-service.ts

--- modules/it/peripheral/services/scanner-service.ts
+++ modules/it/peripheral/services/scanner-service.ts
@@ -1,37 +1,70 @@
 import { createClient } from "@/lib/supabase/client";
 import { calculateScore } from "./scoring-service";
+
 const supabase = createClient();
+
+function normalizeScanTime(value: unknown): string | null {
+
+  if (!value || typeof value !== "string") {
+    return null;
+  }
+
+  const date = value.trim();
+
+  // Format:
+  // 2026-09-08 09.22.17
+  //
+  // menjadi:
+  // 2026-09-08 09:22:17
+  const normalized = date.replace(
+    /^(\d{4}-\d{2}-\d{2})\s+(\d{2})\.(\d{2})\.(\d{2})$/,
+    "$1 $2:$3:$4"
+  );
+
+  return normalized;
+}
 
 export async function uploadScanner(
   peripheralId: number,
   scanner: any
 ) {
 
-   const score =
+  const score =
     calculateScore(scanner);
+
+  const scanTime =
+    normalizeScanTime(scanner.scan_time);
+
+  console.log("SCAN TIME ASLI:", scanner.scan_time);
+  console.log("SCAN TIME NORMALIZED:", scanTime);
+
   return supabase
     .from("peripherals")
-    
+
     .update({
 
       scanner_version:
         scanner.scanner_version,
 
       last_scan_at:
-        scanner.scan_time,
+        scanTime,
 
       last_scan_by:
         scanner.computer.username,
 
       hardware: {
 
-        cpu: scanner.cpu,
+        cpu:
+          scanner.cpu,
 
-        memory: scanner.memory,
+        memory:
+          scanner.memory,
 
-        storage: scanner.storage,
+        storage:
+          scanner.storage,
 
-        gpu: scanner.gpu,
+        gpu:
+          scanner.gpu,
 
         motherboard:
           scanner.motherboard,
@@ -45,36 +78,38 @@ export async function uploadScanner(
         network:
           scanner.network,
 
-          
-
       },
 
       software:
         scanner.software,
 
-         server_services:
-    scanner.server_services,
+      server_services:
+        scanner.server_services,
 
-score:
-  score.total,
+      score:
+        score.total,
 
-status:
-  score.status,
+      status:
+        score.status,
 
-score_detail: {
+      score_detail: {
 
-  cpu: score.cpu,
+        cpu:
+          score.cpu,
 
-  ram: score.ram,
+        ram:
+          score.ram,
 
-  storage: score.storage,
+        storage:
+          score.storage,
 
-  windows: score.windows,
+        windows:
+          score.windows,
 
-  gpu: score.gpu,
+        gpu:
+          score.gpu,
 
-},
-
+      },
 
     })
     .eq("id", peripheralId);
